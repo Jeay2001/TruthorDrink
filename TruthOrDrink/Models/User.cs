609d7fd4b473288b59cda49027d76448b5f77486
Jeay <jeay2001@gmail.com>
2024-12-17 10:01:53 +0100

    Made every page a scrollview because of smaller phone screens

---
+++ TruthOrDrink/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace TruthOrDrink.Models
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Column("Name"), Indexed, NotNull]
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string QrCode { get; set; }
+
+        public int FriendId { get; set; }
+
+        public List<User> Friends { get; set; }
+    }
+}
